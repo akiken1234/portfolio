867ae760d48f6eecad0502efe6ba819a15008b4c
秋本健作 <akimotokensaku@akimotokensakunoMacBook-Pro.local>
2021-09-03 10:39:24 +0900

    dockerの設定

--- app/main.go
+++ app/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	// fmt.Printf("hello, world\n")
-	fmt.Println("Hello world")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{})
+	})
+
+	router.Run()
 }
